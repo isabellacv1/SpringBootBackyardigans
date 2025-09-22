385c1d5f7eb48952d908dbac8d9df9d0a406a0ef
Rubén Darío Marquinez Rincon <86062815+DarioM70@users.noreply.github.com>
2025-09-21 20:35:24 -0500

    set HealthCheckController

--- src/main/java/org/example/taller2/controller/HealthCheckController.java
+++ src/main/java/org/example/taller2/controller/HealthCheckController.java
@@ -2,13 +2,15 @@ package org.example.taller2.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RequestMapping("/health")
 public class HealthCheckController {
-    @GetMapping("/health")
+    @GetMapping("/")
     public ResponseEntity<?> healthCheck() {
         return ResponseEntity.status(200).body(
                 Map.of("status", "UP")
